e6ceb737e7e87673ecd02c9c3aa35a31456d6727
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-02-16 00:11:38 +0800

    Rewrite file framework

--- src/sender.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (protocols, host, port, timeout, threads, packet, broadcast) {
-
-    const exec = require(`./protocols/${protocols.toLowerCase()}`);
-    exec(host, port, timeout, threads, packet, broadcast);
-}
